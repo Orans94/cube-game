909c1531266d9e219376d5ee85e400dcba957712
Orans94 <orans94@gmail.com>
2019-03-18 15:11:57 +0200

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb; // just a ref to RigidBody
+    public float fowardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // add a foward force
+        rb.AddForce(0, 0, fowardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
